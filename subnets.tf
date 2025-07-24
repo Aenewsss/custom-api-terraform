105bac1499d9ce0b8a72684b07d8b56d23813960
Aenewsss <aenamartinelli@gmail.com>
2025-07-24 09:12:17 -0300

    feat: igw to public subnets

--- subnets.tf
+++ subnets.tf
@@ -7,8 +7,8 @@ resource "aws_subnet" "public_1a" {
   tags = merge(
     local.tags,
     {
-      Name                  = "${var.project}-subnet-public-1a",
-      "kubernets./role/elb" = 1
+      Name                    = "${var.project}-public-subnet-1a",
+      "kubernets.io/role/elb" = 1
     }
   )
 }
@@ -22,38 +22,45 @@ resource "aws_subnet" "public_1b" {
   tags = merge(
     local.tags,
     {
-      Name                  = "${var.project}-subnet-public-1b",
-      "kubernets./role/elb" = 1
+      Name                    = "${var.project}-public-subnet-1b",
+      "kubernets.io/role/elb" = 1
     }
   )
 }
 
 resource "aws_subnet" "private_1a" {
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, 2)
-  availability_zone       = "${data.aws_region.current.region}a"
-  map_public_ip_on_launch = true
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, 2)
+  availability_zone = "${data.aws_region.current.region}a"
 
   tags = merge(
     local.tags,
     {
-      Name                  = "${var.project}-subnet-private-1a",
-      "kubernets./role/elb" = 1
+      Name                             = "${var.project}-private-subnet-1a",
+      "kubernets.io/role/internal-elb" = 1
     }
   )
 }
 
 resource "aws_subnet" "private_1b" {
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, 3)
-  availability_zone       = "${data.aws_region.current.region}b"
-  map_public_ip_on_launch = true
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, 3)
+  availability_zone = "${data.aws_region.current.region}b"
 
   tags = merge(
     local.tags,
     {
-      Name                  = "${var.project}-subnet-private-1b",
-      "kubernets./role/elb" = 1
+      Name                             = "${var.project}-private-subnet-1b",
+      "kubernets.io/role/internal-elb" = 1
     }
   )
+}
+
+resource "aws_route_table_association" "public_1a_rtb_assoc" {
+  route_table_id = aws_route_table.public_rtb.id
+  subnet_id      = aws_subnet.public_1a.id
+}
+resource "aws_route_table_association" "public_1b_rtb_assoc" {
+  route_table_id = aws_route_table.public_rtb.id
+  subnet_id      = aws_subnet.public_1b.id
 }
